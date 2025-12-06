bd63f75ac54f98ec5978476662eb70a2c928778f
Nouh2 <noe.tehraoui1@gmail.com>
2025-12-06 13:49:28 +0100

    fixe

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10285527
+    "zoneId": 10285651
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
